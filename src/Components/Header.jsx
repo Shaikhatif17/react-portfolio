b60e7d560456bf10955e8087d9565253075a259e
atif shaikh <shaikhatif456a@gmail.com>
2023-06-14 13:05:27 +0530

    feat - added header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import "../Styles/Header.scss";
 
 const Header = () => {
   return (
-    <div>
-      header rgrrg
-    </div>
+    <nav>
+      <NavContent/>
+    </nav>
   )
+};
+
+const NavContent=()=>{
+    return(
+<>
+<h2>Shaikh Atif. </h2>
+<div>
+    <a href="#home">Home</a>
+    <a href="#word">Word</a>
+    <a href="#timeline">Experience</a>
+    <a href="#services">services</a>
+    <a href="#testimonial">Testimonial</a>
+    <a href="#contact">Contact</a>
+</div>
+<a href="mailto:shaikhatif456a@gmail.com">
+    <button>Email</button>
+</a>
+</>
+    )
 }
 
 export default Header

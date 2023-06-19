f2c13d40bb860d6128fa1c0e82661b91dde4f7c3
atif shaikh <shaikhatif456a@gmail.com>
2023-06-19 12:51:22 +0530

    refactor - changes some code

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import '../Styles/Home.scss';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div>
+    <div id='home'>
+      <section>
+        <div>
+<motion.h1>
+  Hi, i am Shaikh Atif
+</motion.h1>
+        </div>
+
+      </section>
+      <section></section>
       home
     </div>
   )
